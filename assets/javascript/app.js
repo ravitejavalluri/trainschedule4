2dff05e22a47df31d4f28d8c8ce7413c06f5fa47
ychang21 <isabellhchang@gmail.com>
2016-09-22 14:44:26 -0400

    Added in a welcome message to whoever logs in

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,7 +29,7 @@ firebase.auth().signInWithPopup(provider).then(function(result) {
   var token = result.credential.accessToken;
   // The signed-in user info.
   var user = result.user;
-  // ...
+  $('#user-message').html("Welcome, " + user.displayName + "!");
 }).catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
